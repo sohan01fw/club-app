8ae00d4a4c3ada025fec3e30e2422afedc76e9e0
sohancwact <sohancwactfw@gmail.com>
2023-09-09 09:43:11 +0545

    downloading icons for bottombar and styling it

--- Components/Home-page/Bottombar.tsx
+++ Components/Home-page/Bottombar.tsx
@@ -1,7 +1,29 @@
+import { LinkData } from "@/Data/LinkData";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Bottombar = () => {
-  return <div className="border fixed w-full bottom-0 bg-white">Bottombar</div>;
+  return (
+    <section className="border fixed w-full bottom-0 bg-white h-10 shadow-md rounded-md ">
+      <div className="flex  flex-row gap-5 justify-between m-1 ml-2 mr-2">
+        {LinkData.map((data) => {
+          return (
+            <Link key={data.lable} href={data.route} className="icons ">
+              <Image
+                src={data.imgURL}
+                alt={data.lable}
+                width={28}
+                height={28}
+                className=""
+              />
+              <p className="max-md:hidden">{data.lable}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Bottombar;
---
+++ Data/LinkData.tsx
@@ -0,0 +1,33 @@
+export const LinkData = [
+  {
+    imgURL: "/Icons/home.svg",
+    route: "/",
+    lable: "Home",
+  },
+  {
+    imgURL: "/Icons/search.svg",
+    route: "/search",
+    lable: "search",
+  },
+  {
+    imgURL: "/Icons/heart.svg",
+    route: "/activity",
+    lable: "activity",
+  },
+
+  {
+    imgURL: "/Icons/create.svg",
+    route: "/create",
+    lable: "create",
+  },
+  {
+    imgURL: "/Icons/communities.svg",
+    route: "/communities",
+    lable: "communities",
+  },
+  {
+    imgURL: "/Icons/setting.svg",
+    route: "/setting",
+    lable: "setting",
+  },
+];
