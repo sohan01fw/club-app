c38f6621b363c9de6023ec8a5045f4308a4950ac
sohancwact <sohancwactfw@gmail.com>
2023-09-25 11:58:07 +0545

    fetching post from db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,34 +1,15 @@
-"use client";
 import ClubPost from "@/Components/Home-page/ClubPost";
 import { useSession } from "next-auth/react";
 
-export default function Home() {
+export default async function Home() {
   /*   const { data: session } = useSession();
   console.log(session); */
+  // const userPost = await fetchPost()
   return (
     <main className=" flex flex-col mt-20  sm:ml-28 md:ml-28 md:mr-5 lg:ml-40 xl:ml-40 2xl:ml-10   ">
-      <div className="pagetitle  bg-white mb-8 ">
-        <h2 className=" font-bold text-[30px] m-2  w-40 text-center shadow-md md:w-60 xl:ml-40">
-          Home
-        </h2>
-      </div>
       <div className="posts xl:ml-52 ">
-        <ClubPost />
-        <ClubPost />
-        <ClubPost />
-        <ClubPost />
+        <ClubPost name="sohan" text="hey this is me" image="/faviicon.ico" />
       </div>
-
-      {/* <div>
-        <Link href="/signin">
-          <button>LogIn</button>
-        </Link>
-        <button onClick={() => signOut()}>SignOut</button>
-      </div> */}
-      {/* <Link href="/userprofile">
-        <button>Profile</button>
-      </Link> */}
-      {/*  <ModeToggle /> */}
     </main>
   );
 }
--- lib/actions/UserPost.action.ts
+++ lib/actions/UserPost.action.ts
@@ -2,7 +2,7 @@
 import { revalidatePath } from "next/cache";
 import userProfile from "../Models/userprofile.model";
 import { ConnectToDB } from "../mongoose";
-import userPosts from "../Models/userPosts.model";
+import userPosts from "../Models/userposts.model";
 
 type params = {
   text: string;
@@ -35,4 +35,33 @@ export async function UserPost({ text, author, communityId, path }: params) {
   } catch (error: any) {
     throw new Error("Error while creating posts", error.message);
   }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  const skipAmount = (pageNumber - 1) * pageSize;
+  //fetching post that have no parents(this is used to get the top level post which doesn't have parent)
+  const getPostsQuery = await userPosts
+    .find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: userProfile })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: userProfile,
+        select: "_id username profile_pic parentId",
+      },
+    });
+
+  const totalPostCount = await userPosts.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = getPostsQuery;
+
+  const isNext = totalPostCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
