e60d0ecb3795613dc74cc9dd8b57e44366bf1eca
sohancwact <sohancwactfw@gmail.com>
2023-09-27 13:34:39 +0545

    eliminating unused code from root page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,13 +3,7 @@ import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Logo from "@/Components/Home-page/TopNav";
-import LeftNav from "@/Components/Home-page/SideNavBar/LeftNav";
 import SuggestBar from "@/Components/Home-page/SuggestBar";
-import Bottombar from "@/Components/Home-page/SideNavBar/Bottombar";
-import { Suspense } from "react";
-import { ConnectToDB } from "@/lib/mongoose";
-import { ThemeProvider } from "@/Components/Theme-provider";
-import Sublnav from "@/Components/Home-page/SideNavBar/SubLNav";
 import NavigationBar from "@/Components/Home-page/NavigationBar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,7 +18,6 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  ConnectToDB();
   return (
     <html lang="en">
       <AuthContext>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,7 @@ import { fetchPosts } from "@/lib/actions/UserPost.action";
 
 export default async function Home() {
   const result = await fetchPosts(1, 30);
-  /*   console.log(result);
-   */ return (
+  return (
     <main className=" flex flex-col mt-20  sm:ml-28 md:ml-28 md:mr-5 lg:ml-40 xl:ml-40 2xl:ml-10   ">
       <div className="posts xl:ml-52 ">
         {result.posts.map((post) => {
